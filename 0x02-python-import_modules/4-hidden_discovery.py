ed8e8ac7763926040bd2d1c3c3b9a51bff1c97ad
Arenc10 <arenc.palluqi@yahoo.com>
2022-04-28 05:14:39 -0700

    fix

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,5 +2,6 @@
 if __name__ == '__main__':
     import hidden_4
     prefix = "__"
-    string = '\n'.join([str(item) for item in dir(hidden_4) if not item.startswith(prefix)])
-    print(f"{string}")
+    for el in dir(hidden_4):
+        if "__" not in el:
+            print(el)
