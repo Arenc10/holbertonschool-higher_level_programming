98de4fd9d645b0d6cf629fffc1f677707120a32d
Arenc10 <arenc.palluqi@yahoo.com>
2022-05-02 14:10:20 -0700

    Function that replaces an element at a given idx

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx < 0:
-        return my_list
-    if idx > len(my_list):
-        return my_list
-    if 0 <= idx < len(my_list):
-        cpylist = my_list.copy()
+    cpylist = my_list.copy()
+    if idx < 0 or idx not in range(len(my_list)):
+        return cpylist
+    else:
         cpylist[idx] = element
         return cpylist
-
