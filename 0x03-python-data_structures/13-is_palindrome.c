037fa79a5cab470073b41af4979196f08a12e56b
Arenc10 <arenc.palluqi@yahoo.com>
2022-05-05 00:56:16 -0700

    Is a palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+ * is_palindrome - Function that checks if the list is a palindrome or not
+ * @head: Head of a linked list
+ * Return: 0 if False, 1 if True
+ */
+int is_palindrome(listint_t **head)
+{
+	int size = 0, idx = 0;
+	int list[1024];
+	
+	if (*head == NULL || (*head)->next == NULL)
+		return (1);
+	while (*head != NULL)
+	{
+		list[size] = (*head)->n;
+		*head = (*head)->next;
+		size++;
+	}
+	while (size >= idx)
+	{
+		if (list[size - 1] != list[idx])
+			return (0);
+		size --;
+		idx++;
+	}
+	return (1);
+}
