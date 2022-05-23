516063b0c54a3be45ce371dc10d7f07545668864
Arenc10 <arenc.palluqi@yahoo.com>
2022-05-23 05:41:47 -0700

    Fix

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    """ Addition function
+
+    Args:
+        a int: first operand
+        b int: second operand
+    Return:
+        returns the addition of two ints
+    """
+
+    if type(a) != int and type(a) != float:
+        raise TypeError("a must be an integer")
+    if type(b) != int and type(b) != float:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
