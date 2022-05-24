90b6e0d1f5239837595adfd2cf1ead5f228002c2
Arenc10 <arenc.palluqi@yahoo.com>
2022-05-24 05:30:44 -0700

    Fix

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -21,7 +21,7 @@ class TestMaxInteger(unittest.TestCase):
     def test_string_list(self):
         self.assertEqual(max_integer(["Jack", "Sparrow"]), "Sparrow")
         self.assertEqual(max_integer("Jack"), "k")
-        self.assertEqual(max_integer([""]), None)
+        self.assertEqual(max_integer([""]), "")
 
     def test_float_list(self):
         self.assertEqual(max_integer([1.2, 2.3, 3.4]), 3.4)
