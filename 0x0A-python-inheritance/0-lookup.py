80d9904d7fa0b9384660278b3bb310e54c6cd450
Arenc10 <arenc.palluqi@yahoo.com>
2022-05-30 13:09:10 -0700

    Fix

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""Lookup module"""
+"""Defines a lookup module"""
 
 
 def lookup(obj):
     """Returns the list of available attributes of an object"""
+
     return list(dir(lookup))
