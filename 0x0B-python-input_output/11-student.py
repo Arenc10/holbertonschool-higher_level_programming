c9925175f1889d407f572aadc92282ba46aeab19
Arenc10 <arenc.palluqi@yahoo.com>
2022-06-01 01:08:52 -0700

    Fix

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Defines a python Student class"""
+
+
+class Student:
+    """Representing a student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize the student class
+
+        Args:
+            first_name (str): Student first name.
+            last_name (str): Student last name.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Get a dictionary representation of Student class"""
+        new_dict = {}
+        if attrs is None:
+            return self.__dict__
+        for el in attrs:
+            if el in self.__dict__:
+                new_dict[el] = self.__dict__[el]
+        return new_dict
+    
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student instance"""
+        for k, v in json.items:
+            setattr(self, k, v)
