608ed4eb52e5dcd190a99e255d618d1f0450feca
Arenc10 <arenc.palluqi@yahoo.com>
2022-06-02 06:05:40 -0700

    Square class updated

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -14,8 +14,35 @@ class Square(Rectangle):
         """
         super().__init__(size, size, x, y, id)
         self.size = size
-        self.size = size
-    
+
     def __str__(self):
         """Function that returns a string representation of Square class"""
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
+
+    @property
+    def size(self):
+        """Getter function"""
+        return self.width
+
+    @size.setter
+    def size(self):
+        """Setter function"""
+        self.width = size
+        self.height = size
+
+    def update(self, *args, **kwargs):
+        """Assign an argument to each attribute"""
+
+        argName = ["id", "size", "x", "y"]
+
+        if args and len(args) != 0:
+            for i in range(len(args)):
+                setattr(self, argName[i], args[i])
+        else:
+            for k, v in kwargs.items():
+                if hasattr(self, key):
+                    setattr(self, key, value)
+
+    def to_dictionary(self):
+        """Returns a dictionary representation of the Square class"""
+        return {"id": self.id, "size": self.size, "x": self.x, "y": self.y}
