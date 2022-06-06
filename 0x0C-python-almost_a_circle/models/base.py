b5fa14d65ad9f421f0617e77e8b038c2bba2aacd
Arenc10 <arenc.palluqi@yahoo.com>
2022-06-06 01:44:26 -0700

    fix

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import json
 """Defining a Base class"""
 
 
@@ -23,3 +24,35 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Returns the JSON string representations of a obj"""
+        if list_dictionaries is None:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Function that writes the json string representation to a file"""
+        lst = []
+        filename = cls.__name__ + ".json"
+        if list_objs is not None:
+            for el in list_objs:
+                lst.append(el.to_dictionary())
+        with open(filename, "w", encodeing="utf-8") as f:
+            f.write(cls.to_json_string(new_list))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Function that returns a json string to dictionary"""
+        if json_string is None:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Function that updates by adding class method"""
+        testInstance = cls(1, 1) if cls.__name__ = "Rectangle" else cls(1)
+        testInstance.update(**dictionary)
+        return testInstance
