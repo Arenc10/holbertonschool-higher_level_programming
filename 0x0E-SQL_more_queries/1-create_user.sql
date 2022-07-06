60f751fa9dac033458121ef06dd82b167530f1e1
Arenc10 <arenc.palluqi@yahoo.com>
2022-07-06 03:04:07 -0700

    Fix

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- Crete a user and set password
-CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
