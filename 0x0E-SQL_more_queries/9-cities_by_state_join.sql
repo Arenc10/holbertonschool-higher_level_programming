8c417c2429c26a22d2fe1333ff0e5911156f148f
Arenc10 <arenc.palluqi@yahoo.com>
2022-07-06 05:05:57 -0700

    Fix

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- Script that uses join method
-SELECT `cities.id`, `cities.name`, `states.name`
-FROM `cities` INNER JOIN `states`
-ON `cities.state_id` = `states.id`
-ORDER BY `cities.id` ASC;
+SELECT cities.id, cities.name, states.name
+FROM cities INNER JOIN states
+ON cities.state_id = states.id
+ORDER BY cities.id ASC;
