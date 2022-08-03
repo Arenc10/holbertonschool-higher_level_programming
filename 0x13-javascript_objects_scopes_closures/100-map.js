3e89a605e8e243e074d94e1f637ae9e993514d3f
Arenc10 <arenc.palluqi@yahoo.com>
2022-08-03 02:26:20 -0700

    Fix

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const myList = require('./100-data').list
+const myList = require('./100-data').list;
 const newList = myList.map((el, index) => el * index);
 console.log(myList);
 console.log(newList);
