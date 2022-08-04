921958d112ed3f845d303f437b43ccbf7e257f34
Arenc10 <arenc.palluqi@yahoo.com>
2022-08-04 05:07:39 -0700

    First commits

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''Filters states by user input from MySQL database hbtn_0e_0_usa'''
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3])
+    state = sys.argv[4]
+    query = "SELECT * FROM states WHERE name = '%s'"
+    cur = db.cursor()
+    cur.execute(query, (state, ))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
