32bb86131de8f1a215d8a037f921aca2e81c9e02
Arenc10 <arenc.palluqi@yahoo.com>
2022-08-06 09:22:45 -0700

    Fix

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -12,9 +12,11 @@ if __name__ == "__main__":
                                   sys.argv[2],
                                   sys.argv[3]),
                            pool_pre_ping=True)
+    Base.metadata.create_all(engine)
     session = sessionmaker(bind=engine)()
     res = session.query(State).filter_by(name=sys.argv[4]).first()
     if res:
         print(res.id)
-    print("Not found")
+    else:
+        print("Not found")
     session.close()
