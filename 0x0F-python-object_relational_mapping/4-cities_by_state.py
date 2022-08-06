8cb12d01cd30a31c7a8d72d262173818bea07b61
Arenc10 <arenc.palluqi@yahoo.com>
2022-08-06 09:18:35 -0700

    Fix

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-'''Filters states by user input from MySQL database hbtn_0e_0_usa'''
+'''Script that filter states by user input from MySQL database hbtn_0e_0_usa'''
 import sys
 import MySQLdb
 
