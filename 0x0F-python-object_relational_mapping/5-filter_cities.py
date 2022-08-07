8a25af241e35614b833e1668c4206836e0458b90
Arenc10 <arenc.palluqi@yahoo.com>
2022-08-07 10:18:47 -0700

    Fix

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 '''Filters states by user input from MySQL database hbtn_0e_0_usa'''
-
 import sys
 import MySQLdb
 
+
 if __name__ == "__main__":
+
     db = MySQLdb.connect(
             host="localhost",
             port=3306,
