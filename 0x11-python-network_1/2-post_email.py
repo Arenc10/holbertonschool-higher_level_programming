047da0179a6c5cd86de5c52ec61132c6baff7825
Arenc10 <arenc.palluqi@yahoo.com>
2022-08-19 06:04:31 -0700

    POST request to the passed URL

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Sends a POST request to the passed URL"""
+import sys
+import urllib.request
+import urllib.parse
+
+
+url = sys.argv[1]
+value = {"email": sys.argv[2]}
+data = urllib.parse.urlencode(value)
+data = data.encode('ascii')
+req = urllib.request.Request(url, data)
+with urllib.request.urlopen(req) as response:
+    page = response.read()
+print(page.decode("utf-8"))
