ceba8b9036dfd26b4bee8f4dd7a9ae69737650d4
Arenc10 <arenc.palluqi@yahoo.com>
2022-08-19 03:28:36 -0700

    Displaying the value of x-req-id

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Sends request to URL and displays the value of the X-request-Id"""
+import urllib.request
+import sys
+
+
+url = sys.argv[1]
+
+req = urllib.request.Request(url)
+with urllib.request.urlopen(req) as response:
+        print(response.headers.get('X-Request-Id'))
