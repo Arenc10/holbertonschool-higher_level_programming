c1424df9e5a26d5be10d4585dfc53aa40f53daae
Arenc10 <arenc.palluqi@yahoo.com>
2022-09-14 07:29:01 -0700

    Starwars count

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const axios = require('axios');
+let count = 0;
+
+axios.get(process.argv[2])
+  .then(function (response) {
+    response.data.results.forEach(element => {
+      element.characters.forEach(movies => {
+        if (movies.includes('18')) {
+          count++;
+        }
+      });
+    });
+    console.log(count);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
